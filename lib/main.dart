c3f238c56d50cc1aa7825cd2a438cc6e5c3a2de9
alib1989 <alibahjat@Alis-Laptop.local>
2025-01-11 16:11:44 +0300

    create assets file

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
 import 'dart:async';
-
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:get_storage/get_storage.dart';
-
 import 'app_module.dart';
 import 'app_widget.dart';
 import 'package:core/core.dart';
+
 Future<void> main() async {
   await GetStorage.init();
   // WidgetsFlutterBinding.ensureInitialized();
